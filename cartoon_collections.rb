6b25fd5d4dc0900960cf51509ea66e22e380d470
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-17 10:46:38 -0400

    commit tree 0e0d52bba74be6140343527cf9fd48af443a1904

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.map.with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet long_planeteer_calls
+  long_planeteer_calls.map{|call| "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls array_of_calls
+  if array_of_calls.find{|call| call.length > 4} 
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese array
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |str|
+    cheese_types.include? str
+  end
 end
+
+snacks = ["crackers", "gouda", "thyme"]
+puts find_the_cheese(snacks)
+
